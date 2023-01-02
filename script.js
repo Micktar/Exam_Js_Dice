01ab4ebbf3ab4d8055a2f6546b1eaf0e5c14f23a
Micktar <micktar69@gmail.com>
2023-01-02 13:05:20 +0100

    scores initialized to 0

--- script.js
+++ script.js
@@ -2,16 +2,22 @@
 /* ******************** SCRIPT POUR UN JEU DE DÉ ***************************** */
 
 /* -------------------------- Déclaration des variables du GAME PLAY------------------------ */
-let gamePlay;
+let gamePlay, scores, roundScores;
 /* let playerName_1 = prompt('Quel est le nom du Joueur 1')
 let playerName_2 = prompt('Quel est le nom du Joueur 2') */
 
 /* -------------------------- Fonction d'initialisation du GAME PLAY------------------------ */
 let init = () => {
-  
+  scores = [0,0];
+  roundScores = 0;
   gamePlay = true;
 
   document.querySelector('.dice').style.display = 'none';
+  
+  document.getElementById('score_1').textContent = '0';
+  document.getElementById('score_2').textContent = '0';
+  document.getElementById('current_1').textContent = '0';
+  document.getElementById('current_2').textContent = '0';
 }
 
 /* ------------------------- Fonction pour lancer le dé ---------------------------------- */ 
