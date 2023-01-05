6d7fce11ac72d2ffce6bb765d530c62f6d488854
Micktar <micktar69@gmail.com>
2023-01-05 11:44:31 +0100

    the game seems to be functional

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 
 /* -------------------------- Déclaration des variables du GAME PLAY------------------------ */
 let scores, roundScore, currentPlayer, gamePlay;
-/* let playerName_1 = prompt('Quel est le nom du Joueur 1')
-let playerName_2 = prompt('Quel est le nom du Joueur 2') */
+let playerName_1 = prompt('Quel est le nom du Joueur 1')
+let playerName_2 = prompt('Quel est le nom du Joueur 2')  
 
 /* -------------------------- Fonction d'initialisation du GAME PLAY------------------------ */
 let init = () => {
@@ -19,8 +19,14 @@ let init = () => {
   document.getElementById('score_1').textContent = '0';
   document.getElementById('current_0').textContent = '0';
   document.getElementById('current_1').textContent = '0';
-  document.getElementById('player_1').textContent = 'Player 1';
-  document.getElementById('player_2').textContent = 'Player 2';
+  document.getElementById('player_0').textContent = playerName_1;
+  document.getElementById('player_1').textContent = playerName_2;
+
+  document.querySelector('.player_one_control').classList.remove('winner');
+  document.querySelector('.player_two_control').classList.remove('winner');
+  document.querySelector('.player_one_control').classList.remove('active');
+  document.querySelector('.player_two_control').classList.remove('active');
+  document.querySelector('.player_one_control').classList.add('active');
 }
 
 /* ------------------------- Fonction pour lancer le dé ---------------------------------- */ 
@@ -45,13 +51,14 @@ let roll = document.querySelector('#btn_roll').addEventListener('click', () => {
 
       document.querySelector('#current_' + currentPlayer).textContent = roundScore;
     } else {
-      nextPlayer(dice) // créer function nextPlayer
+      nextPlayer() 
     }
     console.log(dice)
- 
   }
 
 })
+
+/* ------------------------- Fonction pour le bouton Hold ---------------------------------- */ 
 
 let hold = document.querySelector('.btn-hold').addEventListener('click', () => {
   if(gamePlay){
@@ -59,17 +66,35 @@ let hold = document.querySelector('.btn-hold').addEventListener('click', () => {
 
     document.querySelector('#score_' + currentPlayer).textContent = scores[currentPlayer];
 
-   
+    if(scores[currentPlayer] >= 10) {
+      document.querySelector('#player_' + currentPlayer).textContent = "Vainqueur!";
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player_' + currentPlayer + '_control').classList.add('winner');
+      document.querySelector('.player_' + currentPlayer + '_control').classList.remove('active');
+      gamePlay = false;
+    } else {
+      nextPlayer();
+    }
 
     console.log(scores)
   }
 })
 
 
+/* ------------------------- Fonction pour le joueur suivant ---------------------------------- */ 
+let nextPlayer = () => {
+  currentPlayer === 0 ? currentPlayer = 1 : currentPlayer = 0 ;
+  roundScore = 0;
+  document.getElementById('current_0').textContent = '0';
+  document.getElementById('current_1').textContent = '0';
+
+  document.querySelector('.player_one_control').classList.toggle('active');
+  document.querySelector('.player_two_control').classList.toggle('active');
+}
 
 
 
-
+/* ------------------------- Fonction pour une nouvelle partie ---------------------------------- */ 
 
 document.querySelector('#btn_newGame').addEventListener('click', init)
 
